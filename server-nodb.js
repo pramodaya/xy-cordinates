9bba98c5a183aa103c30d3aad004322265ce5e11
pramodaya <pramodayajayalath@gmail.com>
2023-03-27 03:05:40 +0300

    Update server-nodb.js

--- server-nodb.js
+++ server-nodb.js
@@ -2,27 +2,12 @@ const express = require('express');
 const http = require('http');
 const axios = require('axios');
 const { Server } = require('socket.io');
-const { MongoClient } = require('mongodb');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const uri = 'mongodb://localhost:27017/';
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const PORT = process.env.PORT || 3000;
-const collectionName = 'coordinates';
-
-async function connectToDB() {
-  try {
-    await client.connect();
-    console.log('Connected to MongoDB');
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-connectToDB();
 
 app.use(express.static('public'));
 
@@ -43,13 +28,6 @@ io.on('connection', (socket) => {
       if (data.startsWith('data:')) {
         const coordinates = JSON.parse(data.slice(5).trim());
         socket.emit('newCoordinates', coordinates);
-
-        // const collection = client.db('test').collection(collectionName);
-        // const timestamp = new Date();
-        // const coordinatesWithTimestamp = { ...coordinates, timestamp };
-        // collection.insertOne(coordinatesWithTimestamp)
-        //   .then(() => console.log('Coordinates inserted into MongoDB'))
-        //   .catch((err) => console.error('Error inserting coordinates: ', err));
       }
     });
   });
