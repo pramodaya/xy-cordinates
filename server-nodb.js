9aafef1baadbb7a1fb065c8598f8a68e1a477ef1
pramodaya <pramodayajayalath@gmail.com>
2023-03-27 03:19:55 +0300

    Update server-nodb.js

--- server-nodb.js
+++ server-nodb.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 300;
 
 app.use(express.static('public'));
 
